96db53266895c2dcd782e919865ca150ea51b92d
Kachi <162593070+kachi-dev@users.noreply.github.com>
2024-03-12 13:39:35 +0000

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -128,7 +128,6 @@ local SaveManager = {} do
 
 		for _, option in next, decoded.objects do
 			if self.Parser[option.type] then
-				warn(option.idx, option.value)
 				task.spawn(self.Parser[option.type].Load, option.idx, option) -- task.spawn() so the config loading wont get stuck.
 			else
 				print(option.idx, 'has no parser of type', option.type)
